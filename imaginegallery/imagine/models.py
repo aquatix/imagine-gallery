2264842a8c4c16229b1ed2327750700db465a35f
Michiel Scholten <michiel@diginaut.net>
2016-10-12 15:59:48 +0200

    Django refactoring

--- imaginegallery/imagine/models.py
+++ imaginegallery/imagine/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
@@ -14,15 +16,13 @@ class BaseModel(models.Model):
 
 class Collection(BaseModel):
     """Collection of images in a certain base_dir"""
-    name = CharField()
-    slug = CharField(null=True)
-    base_dir = CharField()
+    name = models.CharField()
+    slug = models.CharField(null=True)
+    base_dir = models.CharField()
 
-    description = CharField(null=True)
+    description = models.CharField(null=True)
 
-    password = CharField(null=True)  # TODO: something with encryption, preferably through a function in the model
-
-    added_at = DateTimeField(default=datetime.datetime.now())
+    password = models.CharField(null=True)  # TODO: something with encryption, preferably through a function in the model
 
     def __unicode__(self):
         return '{0} ({1})'.format(self.name, self.base_dir)
@@ -30,10 +30,10 @@ class Collection(BaseModel):
 
 class Directory(BaseModel):
     """Directory/collection umbrella object"""
-    directory = CharField()
-    collection = ForeignKeyField(Collection)
+    directory = models.CharField()
+    collection = models.ForeignKey(Collection)
 
-    added_at = DateTimeField(default=datetime.datetime.now())
+    added_at = models.DateTimeField(default=datetime.datetime.now())
 
     def get_filepath(self, filename):
         return '{0}{1}'.format(self.directory, filename)
@@ -48,28 +48,28 @@ class Image(BaseModel):
     IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'bmp', 'gif', 'cr2']
     IMAGE_EXTENSIONS_RAW = ['cr2']
 
-    directory = ForeignKeyField(Directory, related_name='parent')
-    filename = CharField()
-    file_ext = CharField()
-    filetype = CharField(null=True)
-    filesize = IntegerField(default=-1)
+    directory = models.ForeignKey(Directory, related_name='parent')
+    filename = models.CharField()
+    file_ext = models.CharField()
+    filetype = models.CharField(null=True)
+    filesize = models.IntegerField(default=-1)
 
     # Datetime stamps
-    file_modified = DateTimeField(null=True)
-    exif_modified = DateTimeField(null=True)
+    file_modified = models.DateTimeField(null=True)
+    exif_modified = models.DateTimeField(null=True)
     # Contains either file_modified or exif_modified, used for filtering into events and such:
-    filter_modified = DateTimeField(null=True)
+    filter_modified = models.DateTimeField(null=True)
 
-    added_at = DateTimeField(default=datetime.datetime.now())
-    title = TextField(default='')
-    description = TextField(default='')
-    is_visible = BooleanField(default=True)
+    added_at = models.DateTimeField(default=datetime.datetime.now())
+    title = models.TextField(default='')
+    description = models.TextField(default='')
+    is_visible = models.BooleanField(default=True)
 
-    width = IntegerField(default=-1)
-    height = IntegerField(default=-1)
+    width = models.IntegerField(default=-1)
+    height = models.IntegerField(default=-1)
 
-    image_hash = CharField(null=True)
-    thumb_hash = CharField(null=True)
+    image_hash = models.CharField(null=True)
+    thumb_hash = models.CharField(null=True)
 
     # TODO: GPS geotag
     # http://stackoverflow.com/questions/10799366/geotagging-jpegs-with-pyexiv2
@@ -85,25 +85,25 @@ class Image(BaseModel):
 
 
     def get_similar(self):
-        return Image.select().where(Image.image_hash==self.image_hash)
+        return Image.objects.filter(Image.image_hash==self.image_hash)
 
 
     def modified_datetime(self):
         """Returns file_modified or exif_modified, whichever is more accurate, redundant over filter_modified"""
-        if exif_modified:
-            return exif_modified
+        if self.exif_modified:
+            return self.exif_modified
         else:
-            return file_modified
+            return self.file_modified
 
 
     @classmethod
     def is_image(cls, filename):
         """Is file in `filename` of a supported image type"""
         f = filename.lower()
-        for ext in IMAGE_EXTENSIONS:
+        for ext in cls.IMAGE_EXTENSIONS:
             if f.endswith(ext):
                 return True
-        for ext in IMAGE_EXTENSIONS_RAW:
+        for ext in cls.IMAGE_EXTENSIONS_RAW:
             if f.endswith(ext):
                 return True
         return False
@@ -115,33 +115,33 @@ class Image(BaseModel):
 
 class ExifItem(BaseModel):
     """Piece of exif info of a certain Image"""
-    image = ForeignKeyField(Image)
-    key = CharField()
-    value_str = CharField(null=True)
-    value_int = IntegerField(null=True)
-    value_float = FloatField(null=True)
+    image = models.ForeignKey(Image)
+    key = models.CharField()
+    value_str = models.CharField(null=True)
+    value_int = models.IntegerField(null=True)
+    value_float = models.FloatField(null=True)
 
-    def get_value(as_type='str'):
-        if value_str:
-            return value_str
-        elif value_int:
-            return value_int
-        elif value_float:
-            return value_float
+    def get_value(self, as_type='str'):
+        if self.value_str:
+            return self.value_str
+        elif self.value_int:
+            return self.value_int
+        elif self.value_float:
+            return self.value_float
         else:
             return None
 
 
 class Comment(BaseModel):
     """Comment on an image"""
-    image = ForeignKeyField(Image)
-    name = CharField()
-    email = CharField()
-    comment = CharField()
+    image = models.ForeignKey(Image)
+    name = models.CharField()
+    email = models.CharField()
+    comment = models.CharField()
 
 
 class Event(BaseModel):
     """Timeframe in which something happened, enabling grouping of images"""
-    title = CharField()
-    start_datetime = DateTimeField()
-    end_datetime = DateTimeField()
+    title = models.CharField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
