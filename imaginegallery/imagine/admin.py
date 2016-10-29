42590e38ea7339fb5d40dd247d8d1e77421a0f1a
Michiel Scholten <michiel@diginaut.net>
2016-10-29 11:07:08 +0200

    Admin for the models

--- imaginegallery/imagine/admin.py
+++ imaginegallery/imagine/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from imagine.models import Collection, Directory, Image, ExifItem, Comment, Event
 
-# Register your models here.
+
+class CollectionAdmin(admin.ModelAdmin):
+    pass
+
+
+class DirectoryAdmin(admin.ModelAdmin):
+    pass
+
+
+class ImageAdmin(admin.ModelAdmin):
+    pass
+
+
+class ExifItemAdmin(admin.ModelAdmin):
+    pass
+
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Directory, DirectoryAdmin)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(ExifItem, ExifItemAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Event, EventAdmin)
