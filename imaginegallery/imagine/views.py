c7cf7bd5a173532b0fead5937dc5086577a36237
Michiel Scholten <michiel@diginaut.net>
2016-11-06 20:56:48 +0100

    Template for collections overview (gallery index page)

--- imaginegallery/imagine/views.py
+++ imaginegallery/imagine/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Collection
 
 def index(request):
-    collections = Collection.objects.all()
-    output = ', '.join([c.title for c in collections])
-    return HttpResponse(output)
+    collection_list = Collection.objects.all()
+    template = loader.get_template('collection/index.html')
+    context = {
+        'collection_list': collection_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def collection_detail(request, collection_slug):
