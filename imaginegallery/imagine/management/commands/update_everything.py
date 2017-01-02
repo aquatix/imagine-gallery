f99f5b76f5145babd24d26993edeefe91bf29580
Michiel Scholten <michiel@diginaut.net>
2017-01-02 21:15:02 +0100

    Cleanup

--- imaginegallery/imagine/management/commands/update_everything.py
+++ imaginegallery/imagine/management/commands/update_everything.py
@@ -1,5 +1,4 @@
-from django.core.management.base import BaseCommand, CommandError
-from imagine.models import Collection
+from django.core.management.base import BaseCommand
 from imagine.actions import update_everything
 
 class Command(BaseCommand):
