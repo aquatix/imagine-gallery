4ba8a8fb3a16f707565827231a0e85afd19bccc1
Michiel Scholten <michiel@diginaut.net>
2017-01-04 20:25:17 +0100

    Updated wsgi file with env vars needed

--- imaginegallery/imaginegallery/wsgi.py
+++ imaginegallery/imaginegallery/wsgi.py
@@ -19,12 +19,21 @@ os.environ["DJANGO_SETTINGS_MODULE"] = "imaginegallery.settings"
 from django.core.wsgi import get_wsgi_application
 _application = get_wsgi_application()
 
-#env_variables_to_pass = ['DB_NAME', 'DB_USER', 'DB_PASSWD', 'DB_HOST', ]
-env_variables_to_pass = ['IMAGINE_SERVER_HOST', 'IMAGINE_STATIC_ROOT', 'IMAGINE_SECRET_KEY', 'IMAGINE_SITE_TITLE', ]
+env_variables_to_pass = [
+    'IMAGINE_DB_NAME',
+    'IMAGINE_DB_USER',
+    'IMAGINE_DB_PASS',
+    'IMAGINE_DB_HOST',
+    'IMAGINE_DB_PORT',
+    'IMAGINE_SERVER_HOST',
+    'IMAGINE_STATIC_ROOT',
+    'IMAGINE_SECRET_KEY',
+    'IMAGINE_SITE_TITLE',
+]
+
 def application(environ, start_response):
     # pass the WSGI environment variables on through to os.environ
     for var in env_variables_to_pass:
-        #print var
         os.environ[var] = environ.get(var, '')
     #print(environ)
     return _application(environ, start_response)
