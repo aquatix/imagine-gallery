a3ec683c0ce19713ce03de29deaa86d991a2f509
Michiel Scholten <michiel@diginaut.net>
2017-02-03 16:53:11 +0100

    Reverted handlers accidentially committed

--- imaginegallery/imagine/views.py
+++ imaginegallery/imagine/views.py
@@ -15,20 +15,6 @@ def fraction_to_float(fraction):
     return float(sum(Fraction(s) for s in fraction.split()))
 
 
-def handler404(request):
-    response = render_to_response('404.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 404
-    return response
-
-
-def handler500(request):
-    response = render_to_response('500.html', {},
-                                  context_instance=RequestContext(request))
-    response.status_code = 500
-    return response
-
-
 def index(request):
     site_title = settings.SITE_TITLE
 
