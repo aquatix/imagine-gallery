ecd9bab48a1d2ea2680c17e1a20e0238d974ec8d
Michiel Scholten <michiel@diginaut.net>
2017-02-03 21:11:57 +0100

    Query Google GEO API for address info

--- imaginegallery/imagine/actions.py
+++ imaginegallery/imagine/actions.py
@@ -10,6 +10,8 @@ from imagine.util import get_exif_location
 from PIL import Image as PILImage, ImageFile as PILImageFile, ExifTags
 import exifread
 import imagehash
+import json
+import requests
 from utilkit import fileutil, datetimeutil
 
 try:
@@ -41,6 +43,34 @@ def get_filename(directory, filename):
 
     new_filename = filename.replace(directory, '')
     return (new_filename, extension)
+
+
+def save_image_geo_location(image):
+    """Queries Google's GEO API for the location of this Image"""
+
+    response = requests.get('https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}'.format(image.geo_lat, image.geo_lon))
+    json_data = json.loads(response.text)
+
+    # Pick the first one, this is generally the most specific result
+    # https://developers.google.com/maps/documentation/geocoding/intro#ReverseGeocoding
+    image.geo_formatted_address = json_data['results'][0]['formatted_address']
+    for component in json_data['results'][0]['address_components']:
+        if 'route' in component['types']:
+            # 'Street'
+            image.geo_route = component['long_name']
+        elif 'postal_code' in component['types']:
+            image.geo_postal_code = component['long_name']
+        elif 'locality' in component['types']:
+            image.geo_city = component['long_name']
+        elif 'administrative_area_level_1' in component['types']:
+            image.geo_administrative_area_level_1 = component['long_name']
+        elif 'geo_administrative_area_level_2' in component['types']:
+            image.geo_administrative_area_level_2 = component['long_name']
+        elif 'country' in component['types']:
+            image.geo_country = component['long_name']
+            image.geo_country_code = component['short_name']
+
+    image.save()
 
 
 def save_jpg_exif(image, filename):
@@ -121,12 +151,12 @@ def save_image_info(the_image, filename, file_ext):
         the_image.filter_modified = the_image.file_modified
 
     if geo_exif_items:
-        print geo_exif_items
         lat, lon = get_exif_location(geo_exif_items)
         the_image.geo_lat = lat
         the_image.geo_lon = lon
         # TODO: create config item to enable/disable geo lookups
         # Do request to http://maps.googleapis.com/maps/api/geocode/xml?latlng=53.244921,-2.479539&sensor=true
+        save_image_geo_location(the_image)
 
     the_image.save()
 
