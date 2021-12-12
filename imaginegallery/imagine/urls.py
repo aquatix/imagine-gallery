a6a4f8aa9946559779c9230beeeaed6f4ec8d2f1
Michiel Scholten <michiel@diginaut.net>
2021-12-12 15:25:41 +0100

    New URL path format

--- imaginegallery/imagine/urls.py
+++ imaginegallery/imagine/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('$', views.index, name='index'),
-    path('stream/(?P<stream_slug>[-\w]+)/$', views.stream_detail, name='stream_detail'),
+    path('', views.index, name='index'),
+    path('stream/<stream_slug>/', views.stream_detail, name='stream_detail'),
     # ex: /vacation/
-    path('(?P<collection_slug>[-\w]+)/$', views.collection_detail, name='collection_detail'),
+    path('<collection_slug>/', views.collection_detail, name='collection_detail'),
     # /family/home/fun/DCIM_4242.jpg/view
-    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/view/$', views.image_detail, name='image_detail'),
-    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/full/$', views.image_full, name='image_full'),
-    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/max/$', views.image_max, name='image_max'),
-    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/view/$', views.rootdir_image_detail, name='rootdir_image_detail'),
-    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/full/$', views.rootdir_image_full, name='rootdir_image_full'),
-    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/max/$', views.rootdir_image_max, name='rootdir_image_max'),
-    path('(?P<collection_slug>[-\w]+)/(?P<directory>.+)/$', views.directory_detail, name='directory_detail'),
-    path('imagedetail/(?P<imagehash>\w+)/$', views.imagehash_detail, name='imagehash_detail'),
+    path('<collection_slug>/<file_path>/<imagename>/view/', views.image_detail, name='image_detail'),
+    path('<collection_slug>/<file_path>/<imagename>/full/', views.image_full, name='image_full'),
+    path('<collection_slug>/<file_path>/<imagename>/max/', views.image_max, name='image_max'),
+    path('<collection_slug>/<imagename>/view/', views.rootdir_image_detail, name='rootdir_image_detail'),
+    path('<collection_slug>/<imagename>/full/', views.rootdir_image_full, name='rootdir_image_full'),
+    path('<collection_slug>/<imagename>/max/', views.rootdir_image_max, name='rootdir_image_max'),
+    path('<collection_slug>/<directory>/', views.directory_detail, name='directory_detail'),
+    path('imagedetail/<imagehash>/', views.imagehash_detail, name='imagehash_detail'),
 ]
--- imaginegallery/imaginegallery/urls.py
+++ imaginegallery/imaginegallery/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
-    path('logout/$', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), {'next_page': '/'}, name='logout'),
     path('admin/', admin.site.urls),
-    path('/', include('imagine.urls')),
+    path('', include('imagine.urls')),
 ]
