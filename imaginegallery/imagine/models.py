e0764932c1ea52adb602226182703a3f5da270a1
Michiel Scholten <michiel@diginaut.net>
2021-12-12 15:26:03 +0100

    Use general gettext

--- imaginegallery/imagine/models.py
+++ imaginegallery/imagine/models.py
@@ -1,6 +1,6 @@
 import os
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import gettext as _
 
 #from django_extensions.db.fields import AutoSlugField
 
