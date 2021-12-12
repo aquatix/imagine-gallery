a4c11e7c397009260b8f0f437934f51f5f2218bc
Michiel Scholten <michiel@diginaut.net>
2021-12-12 14:26:40 +0100

    Refactor 'url' into 'path'

--- imaginegallery/imagine/urls.py
+++ imaginegallery/imagine/urls.py
@@ -1,19 +1,19 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^stream/(?P<stream_slug>[-\w]+)/$', views.stream_detail, name='stream_detail'),
+    path('$', views.index, name='index'),
+    path('stream/(?P<stream_slug>[-\w]+)/$', views.stream_detail, name='stream_detail'),
     # ex: /vacation/
-    url(r'^(?P<collection_slug>[-\w]+)/$', views.collection_detail, name='collection_detail'),
+    path('(?P<collection_slug>[-\w]+)/$', views.collection_detail, name='collection_detail'),
     # /family/home/fun/DCIM_4242.jpg/view
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/view/$', views.image_detail, name='image_detail'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/full/$', views.image_full, name='image_full'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/max/$', views.image_max, name='image_max'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/view/$', views.rootdir_image_detail, name='rootdir_image_detail'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/full/$', views.rootdir_image_full, name='rootdir_image_full'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/max/$', views.rootdir_image_max, name='rootdir_image_max'),
-    url(r'^(?P<collection_slug>[-\w]+)/(?P<directory>.+)/$', views.directory_detail, name='directory_detail'),
-    url(r'^imagedetail/(?P<imagehash>\w+)/$', views.imagehash_detail, name='imagehash_detail'),
+    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/view/$', views.image_detail, name='image_detail'),
+    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/full/$', views.image_full, name='image_full'),
+    path('(?P<collection_slug>[-\w]+)/(?P<file_path>.+)/(?P<imagename>.+)/max/$', views.image_max, name='image_max'),
+    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/view/$', views.rootdir_image_detail, name='rootdir_image_detail'),
+    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/full/$', views.rootdir_image_full, name='rootdir_image_full'),
+    path('(?P<collection_slug>[-\w]+)/(?P<imagename>.+)/max/$', views.rootdir_image_max, name='rootdir_image_max'),
+    path('(?P<collection_slug>[-\w]+)/(?P<directory>.+)/$', views.directory_detail, name='directory_detail'),
+    path('imagedetail/(?P<imagehash>\w+)/$', views.imagehash_detail, name='imagehash_detail'),
 ]
--- imaginegallery/imaginegallery/urls.py
+++ imaginegallery/imaginegallery/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import include, path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
-    url(r'^logout/$', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('imagine.urls')),
+    path('login/$', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+    path('logout/$', auth_views.LogoutView, {'next_page': '/'}, name='logout'),
+    path('admin/', admin.site.urls),
+    path('/', include('imagine.urls')),
 ]
