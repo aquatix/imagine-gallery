926f4583a5d08c13fc36017593e661998e660950
Michiel Scholten <michiel@diginaut.net>
2022-07-10 11:22:48 +0200

    Updated to denote imagine-gallery

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='imagine-core', # pip install imagine
-    description='Image archive, core (models)',
+    name='imagine-gallery', # pip install imagine-gallery
+    description='Image archive, core (models) and frontend',
     #long_description=open('README.md', 'rt').read(),
     long_description=long_description,
 
@@ -29,13 +29,12 @@ setup(
     author='Michiel Scholten',
     author_email='michiel@diginaut.net',
 
-    url='https://github.com/aquatix/imagine-core',
+    url='https://github.com/aquatix/imagine-gallery',
     license='Apache',
 
     # as a practice no need to hard code version unless you know program wont
     # work unless the specific versions are used
-    install_requires=['peewee'],
-    # Flask for web
+    install_requires=['django'],
 
     py_modules=['imagine-core'],
 
